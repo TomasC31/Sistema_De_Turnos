0a020d024a1bfe5518ba8c8c5f8b938bb8d8ed07
Tomas Caussa <caussad@gmail.com>
2025-12-27 17:51:57 -0300

    Agregando endpoint de registrar y login, ademas agregué lo de encriptacion de la contraseña

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express'); //Importo express, sirve para crear el servidor
 const app = express(); //Inicializo express, ahora app tiene todas las funcionalidades de express
 
+const bcrypt = require('bcrypt'); //Importo bcrypt, sirve para hashear contraseñas
+
 app.use(express.json()); //Middleware, permite que el servidor entienda los datos JSON que le envío
 
 const reservas = []; //Base de datos provisoria
+const usuarios = []; // guardamos los emails de los usuarios registrados
 
 contadorDeId = 0;
 
@@ -48,6 +51,46 @@ app.post('/reservar', (req, res) => {
     
     res.send(`Reserva confirmada para ${nombreCliente} a las ${horarioElegido}`)
 })
+
+//Para registrar usuario
+app.post("/registrar", async (req, res) => {
+    const {email, password, nombre} = req.body;
+    const indice = usuarios.findIndex(usuario => usuario.email === email); //findIndex devuelve -1 si no encuentra el elemento
+   
+    if(indice === -1){
+        const passwordEncriptada = await bcrypt.hash(password, 10); //Hasheo la contraseña, el 10 es la cantidad de rondas de encriptacion
+       
+        usuarios.push({nombre, email, passwordEncriptada, rol: 'usuario'}); //Guardo el usuario en la "base de datos"
+        res.send(`Te has registrado correctamente con el email: ${email}`)
+    }
+    else{
+        res.status(409).send("Este email ya se encuentra registrado")
+    }
+})
+
+//Para loguear usuario
+app.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+    const indice = usuarios.findIndex(usuario => usuario.email === email); //Busco el usuario en la "base de datos"
+
+    if(indice !== -1){
+        const usuario = usuarios[indice];
+        const passwordCorrecta = await bcrypt.compare(password, usuario.passwordEncriptada); //Comparo la contraseña ingresada con la hasheada
+        
+        if(passwordCorrecta === true)
+        {
+            res.json({mensaje:"Bienvenido", nombre: usuario.nombre, rol: usuario.rol}) //Envio un json con el mensaje de bienvenida y el rol del usuario a el frontend
+        }
+        else{
+            res.status(401).send("Contraseña incorrecta")
+        }
+    } 
+    else{
+        res.status(404).send("Email no encontrado")
+    }
+})
+
+
 
 //Para dar de baja el turno
 app.delete('/cancelar/:id', (req, res) => {
